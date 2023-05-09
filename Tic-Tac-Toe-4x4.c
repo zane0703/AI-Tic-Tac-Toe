6a1fef49719481220e57241ebf5677848050dbb4
Ang Yun, Zane <zane0703@hotmail.com>
2023-05-09 22:16:35 +0800

    added 4x4 tic tac toe

---
+++ Tic-Tac-Toe-4x4.c
@@ -0,0 +1,114 @@
+#include <limits.h>
+#include <stdbool.h>
+#include <string.h>
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+signed char minimax(unsigned char* board, unsigned char maxSymbol, unsigned char minSymbol,  char depth, bool isMaximizing);
+bool EMSCRIPTEN_KEEPALIVE isWinner(unsigned char* board, unsigned char player);
+bool EMSCRIPTEN_KEEPALIVE isBoardFull(unsigned char *board);
+
+int EMSCRIPTEN_KEEPALIVE  smartChoice(unsigned char * board, unsigned char player){
+  /*   ''' Returns a smart choice using an AI algorithm
+    ''' */
+    int bestMove = 0;              // # initialize bestMove
+    unsigned char dupBoard[16];
+    signed char score, bestScore = SCHAR_MIN;
+    int i;
+    memcpy(dupBoard, board, sizeof(unsigned char) * 16);
+    for (i = 0; i< 16; ++i){
+        if (dupBoard[i] != ' ') continue;
+        //# Simulate the move
+        dupBoard[i] = player;
+
+
+        //# Find score using Minimax algorithm
+        score = minimax(dupBoard,        // # use board's copy
+                        player,         // # maximize for Computer (O)
+                        player == 'O'? 'X': 'O',       //  # minimize for Human (X)
+                        1,               // # depth of search tree
+                        false);  //  # is the next move for O
+        
+        //# Undo the move for simulation
+        dupBoard[i] = ' ';
+        
+        //# Update bestScore if appropriate
+        if (score > bestScore){
+            bestScore = score;
+            bestMove = i;
+        }
+    }
+    //# Return the best move
+    return bestMove;
+}
+
+signed char minimax(unsigned char* board, unsigned char maxSymbol, unsigned char minSymbol,  char depth, bool isMaximizing){
+   /*  ''' Minimax algorithm for the recursion
+    ''' */
+    //# Terminal conditions for recursion
+    signed char bestScore = isMaximizing? SCHAR_MIN: SCHAR_MAX , score;
+    char position, value;
+    if (isWinner(board, maxSymbol))
+        return 10 - depth;
+    if (isWinner(board, minSymbol))
+        return depth - 10;
+    if (isBoardFull(board) || depth > 5)
+        return 0;
+    //# You may use the isWinner and isBoardFull functions if you want
+    
+    //# Keep track of scores at this depth
+    //# Go through all available positions
+    for (position = 0; position < 16; ++position){
+        if (board[position] != ' ')
+            continue;
+        ///# Simulate the appropriate move
+        board[position] = isMaximizing?maxSymbol : minSymbol;
+        
+        //# Find the score for the move
+        score = minimax(board, maxSymbol, minSymbol, depth+1, !isMaximizing);
+        if (isMaximizing? score> bestScore: score < bestScore) {
+            bestScore = score;
+        }
+        
+        //# Undo the move for simulation
+        board[position] = ' '; // # Fill in all the missing pieces in this code segment
+        // # Simulate the appropriate move
+        // # Find the score for the move
+        // # Undo the move for simulation
+    }
+    return bestScore;
+    // Return max or min as per the level
+    //return max(scores) if isMaximizing else min(scores)
+    //# Fill in the return logic for recursion as per level
+    
+    //# Remove the following exception when you complete this function
+}
+
+bool isWinner(unsigned char* board, unsigned char player){
+    /* ''' Checks if Player has Won the game
+    '''
+    
+    # Check for 3 valid marks denoting a Win */
+    return (((board[0] == player) && (board[1] == player) && ( board[2] == player) && ( board[3] == player)) || //or # Top Row
+           ((board[4] == player) && ( board[5] == player) && ( board[6] == player) && ( board[7] == player)) ||//or # Middle Top Row
+           ((board[8] == player) && ( board[9] == player) && ( board[10] == player) && ( board[11] == player)) ||//or # Middle Bottom Row
+           ((board[12] == player) && ( board[13]  == player) && ( board[14] == player) && ( board[15] == player)) ||//or # Bottom Row
+           ((board[0] == player) && ( board[4] == player) && ( board[8] == player) && ( board[12] == player)) ||//or # Left Column
+           ((board[1] == player) && ( board[5] == player) && ( board[9]  == player) && ( board[13] == player)) ||//or # Center left Column
+           ((board[2] == player) && ( board[6] == player) && ( board[10]  == player) && ( board[14] == player)) ||//or # Center right Column
+           ((board[3] == player) && ( board[7]  == player) && ( board[11] == player) && ( board[15] == player))||//or # Right Column
+           ((board[0] == player) && ( board[5] == player) && ( board[10] == player) && ( board[15] == player))||//or # Diagonal
+           ((board[3] == player) && ( board[6] == player) && ( board[9] == player) && ( board[12] == player)));   //# Diagonal  
+}
+
+bool isBoardFull(unsigned char *board) {
+    /* ''' Checks if the Board is Full
+    ''' */
+    int i;
+    for (i = 0; i < 16; ++i) {
+        if  (board[i] == ' ') return false;
+    }
+    return true;
+}
--- script.js
+++ script.js
@@ -1,61 +1,167 @@
 let isPlayer = true;
-let btn = [document.getElementById("btn0"), document.getElementById("btn1"), document.getElementById("btn2"), document.getElementById("btn3"), document.getElementById("btn4"), document.getElementById("btn5"), document.getElementById("btn6"), document.getElementById("btn7"), document.getElementById("btn8")];
-let clear = document.getElementById("clear");
-let status = document.getElementById("status"); 
-WebAssembly.instantiateStreaming(fetch("Tic-Tac-Toe.wasm"))
-        .then(wasmIn => {
-            let array = new Int8Array(wasmIn.instance.exports.memory.buffer, 0, 9);
-            array.fill(32);
-            let disableAll = (turn) => {
-                isPlayer = !turn;
-                for (let i = 0; i< 9; ++i ) { 
-                    btn[i].disabled = turn || array[i] != 32;
-                }
-            }
-            for (let i = 0; i< 9; ++i ) {
-                btn[i].disabled
-                btn[i].addEventListener("click",even => {
-                    if (isPlayer) {
-                        if (array[i] == 32) {
-                            array[i] = 79;
-                            even.target.textContent = "O"
-                            even.target.disabled = true;
-                            disableAll(true);
-                                    
-                            if ( wasmIn.instance.exports.isWinner(array, 79)) {
-                                status.textContent = "You win!"
-                                return;
-                            } else if (wasmIn.instance.exports.isBoardFull(array)) {
-                                status.textContent = "Is a draw"
-                                return;
-                            }
-                            let comChoice = wasmIn.instance.exports.smartChoice(array, 88);
-                            array[comChoice] = 88;
-                            btn[comChoice].textContent = "X"
-                            btn[comChoice].disabled = true;
-                            isPlayer = true;
-                            status.textContent = "Computes Move ..."
-                            if ( wasmIn.instance.exports.isWinner(array, 88)) {
-                                status.textContent ="You lost!";
-                                return;
-                            } else if (wasmIn.instance.exports.isBoardFull(array)) {
-                                status.textContent = "Is a draw"
-                                    return;
-                            }
-                            disableAll(false);
-                            status.textContent = "Player Move"
-                        }
-                    }
-                })
-            }
-            clear.addEventListener("click", ()=> {
-                for (let i = 0; i< 9; ++i ) { 
-                    btn[i].textContent = "\xA0";
-                    btn[i].disabled = false;
-                }
-                isPlayer = true;
-                status.textContent = "Player Move"
+let is4x4 = false;
+/**@type {HTMLButtonElement[]} */
+const btn = [];
+for (let i = 0 ; i < 9; ++i) {
+    btn.push(document.getElementById(`btn${i}`))
+}
+Object.freeze(btn)
+/**@type {HTMLButtonElement[]} */
+const btn4x4 = [];
+for (let i = 0 ; i < 16; ++i) {
+    btn4x4.push(document.getElementById(`btn-4x4-${i}`))
+}
+Object.freeze(btn4x4)
+/**@type {HTMLButtonElement} */
+const clear = document.getElementById("clear");
+/**@type {HTMLSpanElement} */
+const statusText = document.getElementById("status"); 
+/**@type {HTMLButtonElement} */
+const changeMode = document.getElementById("changeMode")
 
-                array.fill(32);
+/**@type {HTMLTableElement} */
+const table3x3 = document.getElementById("s3x3")
+/**@type {HTMLTableElement} */
+const table4x4 = document.getElementById("s4x4")
+/**
+ * @param {boolean} disabled
+ * @returns {void}
+ */
+function bottomButtonDisabled (disabled) {
+    clear.disabled = disabled;
+    changeMode.disabled = disabled;
+}
+
+WebAssembly.instantiateStreaming(fetch("Tic-Tac-Toe.wasm"))
+    .then(wasmIn => {
+        let array = new Uint8Array(wasmIn.instance.exports.memory.buffer, 0, 9);
+        array.fill(32);
+        let disableAll = (turn) => {
+            isPlayer = !turn;
+            for (let i = 0; i< 16; ++i ) { 
+                    btn4x4[i].disabled = turn || array[i] != 32;
+            }
+            bottomButtonDisabled(turn);
+        }
+        for (let i = 0; i< 9; ++i ) {
+            btn[i].addEventListener("click",even => {
+                if (!isPlayer && is4x4 && array[i] != 32 ) return;
+                array[i] = 79;
+                even.target.textContent = "O";
+                even.target.disabled = true;
+                disableAll(true);            
+                if (wasmIn.instance.exports.isWinner(array, 79)) {
+                    statusText.textContent = "You win!";
+                    bottomButtonDisabled(false);
+                    return;
+                } else if (wasmIn.instance.exports.isBoardFull(array)) {
+                    statusText.textContent = "Is a draw";
+                    bottomButtonDisabled(false);
+                    return;
+                }
+
+                // computer move
+                statusText.textContent = "Computes Move ...";
+                let comChoice = wasmIn.instance.exports.smartChoice(array, 88);
+                array[comChoice] = 88;
+                btn[comChoice].textContent = "X";
+                btn[comChoice].disabled = true;
+                isPlayer = true;
+                    
+                if ( wasmIn.instance.exports.isWinner(array, 88)) {
+                    status.textContent ="You lost!";
+                    bottomButtonDisabled(false);
+                    return;
+                } else if (wasmIn.instance.exports.isBoardFull(array)) {
+                    status.textContent = "Is a draw";
+                    bottomButtonDisabled(false);
+                    return;
+                }
+                disableAll(false);
+
+                statusText.textContent = "Player Move";
+            })
+        }
+        clear.addEventListener("click", ()=> {
+            if (is4x4) return;
+            for (let i = 0; i< 9; ++i ) { 
+                btn[i].textContent = "\xA0";
+                btn[i].disabled = false;
+            }
+            isPlayer = true;
+            statusText.textContent = "Player Move";
+
+            array.fill(32);
+        });
+    });
+
+    WebAssembly.instantiateStreaming(fetch("Tic-Tac-Toe-4x4.wasm"))
+    .then(wasmIn => {
+        let array = new Uint8Array(wasmIn.instance.exports.memory.buffer, 0, 16);
+        array.fill(32);
+        let disableAll = (turn) => {
+            isPlayer = turn;
+            for (let i = 0; i< 16; ++i ) { 
+                btn4x4[i].disabled = turn || array[i] != 32;
+            }
+            bottomButtonDisabled(turn);
+        }
+        for (let i = 0; i< 16; ++i ) {
+            btn4x4[i].addEventListener("click",even => {
+                if (!isPlayer && !is4x4 && array[i] != 32 ) return;
+                array[i] = 79;
+                even.target.textContent = "O";
+                even.target.disabled = true;
+                disableAll(true);            
+                if (wasmIn.instance.exports.isWinner(array, 79)) {
+                    statusText.textContent = "You win!";
+                    bottomButtonDisabled(false);
+                    return;
+                } else if (wasmIn.instance.exports.isBoardFull(array)) {
+                    statusText.textContent = "Is a draw";
+                    bottomButtonDisabled(false);
+                    return;
+                }
+                statusText.textContent = "Computes Move ...";
+                setTimeout(() =>  {
+                    // computer move
+                
+                let comChoice = wasmIn.instance.exports.smartChoice(array, 88);
+                array[comChoice] = 88;
+                btn4x4[comChoice].textContent = "X";
+                btn4x4[comChoice].disabled = true;
+                isPlayer = true;
+                    
+                if ( wasmIn.instance.exports.isWinner(array, 88)) {
+                    statusText.textContent ="You lost!";
+                    bottomButtonDisabled(false);
+                    return;
+                } else if (wasmIn.instance.exports.isBoardFull(array)) {
+                    statusText.textContent = "Is a draw";
+                    bottomButtonDisabled(false);
+                    return;
+                }
+                disableAll(false);
+
+                statusText.textContent = "Player Move";
+                },0);
+            })
+        }
+        clear.addEventListener("click", ()=> {
+            if (!is4x4) return;
+            for (let i = 0; i< 16; ++i ) { 
+                btn4x4[i].textContent = "\xA0";
+                btn4x4[i].disabled = false;
+            }
+            isPlayer = true;
+            statusText.textContent = "Player Move";
+            
+            array.fill(32);
+        });
+        changeMode.addEventListener("click", () => {
+            table3x3.style.display = is4x4? "block" : "none";
+            table4x4.style.display = is4x4? "none" : "block";
+            changeMode.textContent = is4x4?  "4x4" : "3x3";
+            is4x4 = !is4x4
         })
-})
+    });
